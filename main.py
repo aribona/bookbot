0ba2d61b20a7160edd29efc34938c913c9b5d43f
aribona <arichline97@gmail.com>
2025-06-12 21:51:37 -0600

    Add function to read book text from file and print it in main

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(filepath):
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        return f"Error: The file '{filepath}' not found."
+
+def main():
+    frankenstein_path = "frankenstein.txt"
+    book_text = get_book_text(frankenstein_path)
+    print(book_text)
+
+if __name__ == "__main__":
+    main()
+
